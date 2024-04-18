d4f27728d06006cf8bc23bd20e1975da8184243f
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-04-18 10:28:11 +0300

    Create view logout token

--- api/views/token.py
+++ api/views/token.py
@@ -1,9 +1,10 @@
 from rest_framework import status
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ValidationError
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.serializers.token.get import GetTokenSerializers
+from api.serializers.token.get import GetTokenSerializers, DeleteTokenSerialuzers
 
 
 class GetTockenView(APIView):
@@ -16,3 +17,16 @@ class GetTockenView(APIView):
             })
         except NotFound:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutTockenView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        try:
+            serializer = DeleteTokenSerialuzers(data={}, context={'user': request.user})
+            serializer.is_valid(raise_exception=True)
+            token = serializer.validated_data['obj_token']
+            token.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ValidationError:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
