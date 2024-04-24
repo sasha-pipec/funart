de6d6b21caebe9ed9c4677f0eaf7f1c49363bd23
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-04-24 21:57:33 +0300

    Delete field like from model Like

--- api/services/favourite/create.py
+++ api/services/favourite/create.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 from django import forms
 from rest_framework.generics import get_object_or_404
 from service_objects.fields import ModelField
@@ -7,34 +6,24 @@ from models_app.models import User, Theme
 from models_app.models.favourite.models import Favourite
 
 
-class FavouriteServices(ServiceWithResult):
+class FavouriteCreateServices(ServiceWithResult):
     id = forms.IntegerField()
     user = ModelField(User)
 
     def process(self):
-        self.add_del_favourite()
+        self.favourite_create()
         return self
 
-    def add_del_favourite(self):
-        obj_favourite = Favourite.objects.filter(
+    def favourite_create(self):
+        obj_favourite_search = Favourite.objects.filter(
             theme=self.get_themes(),
             user=self.cleaned_data['user'],
         )
-        if not obj_favourite.exists():
+        if not obj_favourite_search.exists():
             Favourite.objects.create(
                 theme=self.get_themes(),
                 user=self.cleaned_data['user'],
-                status=True
             )
-        else:
-            obj_favourite = obj_favourite.first()
-            if obj_favourite.status == True:
-                obj_favourite.status = False
-                obj_favourite.save()
-            else:
-                obj_favourite.status = True
-                obj_favourite.save()
 
-    @lru_cache
     def get_themes(self):
         return get_object_or_404(Theme, id=self.cleaned_data['id'])
