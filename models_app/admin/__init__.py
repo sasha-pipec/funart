2aa69f0002c8e5b64c6e50bec79137d019171ecd
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-05-03 17:30:48 +0300

    import admin

--- models_app/admin/__init__.py
+++ models_app/admin/__init__.py
@@ -1,4 +1,6 @@
-from models_app.admin.user.resources import UserAdmin
-from models_app.admin.theme.resources import ThemeAdmin
 from models_app.admin.category.resources import CategoryAdmin
 from models_app.admin.coloring.resources import ColoringAdmin
+from models_app.admin.favourite.resources import FavouriteAdmin
+from models_app.admin.like.resources import LikeColoringAdmin, LikeAdmin
+from models_app.admin.theme.resources import ThemeAdmin
+from models_app.admin.user.resources import UserAdmin
