83a6ce79a55b8757b01a86047aa73e7c1d9835e7
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-05-03 17:20:54 +0300

    create views create delete theme

--- api/views/theme_like.py
+++ api/views/theme_like.py
@@ -4,16 +4,16 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from service_objects.services import ServiceOutcome
 
-from api.services.like.create import LikeCreateServices
-from api.services.like.delete import LikeDeleteServices
+from api.services.like_theme.create import LikeCreateServices
+from api.services.like_theme.delete import LikeDeleteServices
 
 
-class ThemesLikeCreateDeleteView(APIView):
+class ThemeLikeCreateDeleteView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, **kwargs):
         ServiceOutcome(LikeCreateServices, {
-            'id': kwargs['id'],
+            'theme_id': kwargs['id'],
             'user': request.user
         })
         return Response(status=status.HTTP_201_CREATED)
