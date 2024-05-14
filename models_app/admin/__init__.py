c920cdb223133b29eb69533e45dd74b010109631
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-05-14 19:13:21 +0300

    Addendum in file __init__ LikeColorinAgdmin, LikeThemeAdmin

--- models_app/admin/__init__.py
+++ models_app/admin/__init__.py
@@ -2,3 +2,9 @@ from models_app.admin.category.resources import CategoryAdmin
 from models_app.admin.coloring.resources import ColoringAdmin
 from models_app.admin.theme.resources import ThemeAdmin
 from models_app.admin.user.resources import UserAdmin
+
+from models_app.admin.category.resources import CategoryAdmin
+from models_app.admin.coloring.resources import ColoringAdmin
+from models_app.admin.like.resources import LikeColorinAgdmin, LikeThemeAdmin
+from models_app.admin.theme.resources import ThemeAdmin
+from models_app.admin.user.resources import UserAdmin
