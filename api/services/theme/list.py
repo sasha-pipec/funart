ae0983349a944a44d2a385b0d54f6ca2a77019bd
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-05-14 19:24:09 +0300

    Sorting by rating,updated_at, likes_count has been added to the _themes method

--- api/services/theme/list.py
+++ api/services/theme/list.py
@@ -8,10 +8,27 @@ from service_objects.services import ServiceWithResult
 from conf.settings.rest_framework import REST_FRAMEWORK
 from models_app.models import Theme
 
+ORDER_BY = {
+    ('rating', True): 'rating',
+    ('rating', False): '-rating',
+    ('updated_at', True): 'updated_at',
+    ('updated_at', False): '-updated_at',
+    ('likes_count', True): 'likes_count',
+    ('likes_count', False): '-likes_count',
+}
+
 
 class ThemeListServices(ServiceWithResult):
+    '''
+    В данный класс в атрибут order_by передаются параметры сортировки (rating, или updated_at, или likes_count),
+    исходя из переданного параметра можно сортировать по рейтингу, либо дате, либо колличеству лайков.
+    В атрибут descending передаются параметры True или False, он указавает как будет сортироваться order_by,
+    True - по возрастанию, False - по убыванию (False стоит по умолчанию)
+    '''
     page = forms.IntegerField(required=False, min_value=1)
     per_page = forms.IntegerField(required=False, min_value=1)
+    order_by = forms.CharField(required=False)
+    descending = forms.BooleanField(required=False)
 
     def process(self):
         self._paginated_themes()
@@ -36,10 +53,19 @@ class ThemeListServices(ServiceWithResult):
 
     @property
     def _themes(self):
+        if not self.cleaned_data['order_by']:
+            return (
+                Theme.objects
+                .prefetch_related('likes')
+                .annotate(likes_count=Count('likes'))
+                .order_by("-rating")
+            )
+
         return (
             Theme.objects
             .prefetch_related('likes')
             .annotate(likes_count=Count('likes'))
-            .all()
-            .order_by("-id")
+            .order_by(
+                ORDER_BY[(self.cleaned_data['order_by'], self.cleaned_data['descending'])]
+            )
         )
