fe9b77d81165f0f529655259e6b300fd3075d47b
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-06-15 15:45:51 +0300

    Adding a preview of the next and previous coloring pages

--- api/services/coloring/get.py
+++ api/services/coloring/get.py
@@ -30,8 +30,80 @@ class ColoringGetService(ServiceWithResult):
                 ))
                 if self.cleaned_data['user_id']
                 else Value(False)
-            )
+            ),
+            next=Value(self._get_next_coloring),
+            previous=Value(self._get_previous_coloring)
         ).get(id=self.cleaned_data['id'])
+
+    @property
+    def _get_next_coloring(self):
+        theme_id = self.get_theme_id()
+        colorings_id_list = list(self.get_colorings_id_list(theme_id))
+        index_coloring = colorings_id_list.index(self.cleaned_data['id'])
+        if index_coloring == len(colorings_id_list) - 1:
+            return self.get_first_coloring_from_the_next_theme(theme_id)
+        else:
+            return colorings_id_list[index_coloring + 1]
+
+    def get_first_coloring_from_the_next_theme(self, theme_id):
+        theme_all_id = self.get_list_id_all_theme()
+        index_theme = theme_all_id.index(theme_id)
+        if index_theme == len(theme_all_id) - 1:
+            next_theme_id = theme_all_id[0]
+            colorings_id_list = self.get_colorings_id_list(next_theme_id)
+            if colorings_id_list.exists():
+                return list(colorings_id_list)[0]
+            else:
+                return self.get_first_coloring_from_the_next_theme(next_theme_id)
+        else:
+            next_theme_id = theme_all_id[index_theme + 1]
+            colorings_id_list = self.get_colorings_id_list(next_theme_id)
+            if colorings_id_list.exists():
+                return list(colorings_id_list)[0]
+            else:
+                return self.get_first_coloring_from_the_next_theme(next_theme_id)
+            # Todo в данном месте не уверен возможно ли так сделать - return self.get_coloring_from_the_following_theme(next_theme_id)
+
+    @property
+    def _get_previous_coloring(self):
+        theme_id = self.get_theme_id()
+        colorings_id_list = list(self.get_colorings_id_list(theme_id))
+        index_coloring = colorings_id_list.index(self.cleaned_data['id'])
+        if index_coloring == 0:
+            return self.get_latest_coloring_from_the_previous_theme(theme_id)
+        else:
+            return colorings_id_list[index_coloring - 1]
+
+    def get_latest_coloring_from_the_previous_theme(self, theme_id):
+        theme_all_id = self.get_list_id_all_theme()
+        index_theme = theme_all_id.index(theme_id)
+        if index_theme == 0:
+            previous_theme_id = theme_all_id[-1]
+            colorings_id_list = self.get_colorings_id_list(previous_theme_id)
+            if colorings_id_list.exists():
+                return list(colorings_id_list)[-1]
+            else:
+                return self.get_latest_coloring_from_the_previous_theme(previous_theme_id)
+        else:
+            previous_theme_id = theme_all_id[index_theme - 1]
+            colorings_id_list = self.get_colorings_id_list(previous_theme_id)
+            if colorings_id_list.exists():
+                return list(colorings_id_list)[-1]
+            else:
+                return self.get_latest_coloring_from_the_previous_theme(previous_theme_id)
+
+    @lru_cache
+    def get_theme_id(self):
+        return Coloring.objects.filter(id=self.cleaned_data['id']).first().theme.id
+
+    @staticmethod
+    @lru_cache
+    def get_list_id_all_theme():
+        return list(Theme.objects.values_list("id", flat=True))
+
+    @staticmethod
+    def get_colorings_id_list(theme_id):
+        return Theme.objects.filter(id=theme_id).first().themes.values_list("id", flat=True)
 
     @property
     def _see_more_themes(self):
