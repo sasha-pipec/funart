680f02e5caceae9317f3f322f713b32331d26888
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-07-17 21:47:32 +0300

    save commit

--- middleware/info.py
+++ middleware/info.py
@@ -1,26 +1,35 @@
 import logging
-from datetime import datetime
 
-# logger = logging.getLogger(__name__)
+
 class FirstInfoMiddleware:
 
     def __init__(self, get_response):
-        self._get_response = get_response
-
+        self.get_response = get_response
 
     def __call__(self, request):
-
         logging.basicConfig(level=logging.INFO, filename="logging_file.log", filemode="w",
                             format="%(asctime)s %(levelname)s %(message)s")
-        # logger.info('START')
 
-        logging.info(f"Domaine name {request.environ['DOMAIN']}")
-        logging.info(f"Body {request.body}")
-        logging.info(f"Headers {request.headers}")
-        logging.info(f"Query_params {request.GET}")
-        logging.info(f"Method {request.method}")
+        # logging.info(f"Domaine name {request.environ['DOMAIN']}")
+        # logging.info(f"Body {request.body}")
+        # logging.info(f"Headers {request.headers}")
+        # logging.info(f"Query_params {request.GET}")
+        # logging.info(f"Method {request.method}")
 
-        response = self._get_response(request)
+        request_log = {
+            'type': 'request',
+            'host': request.environ['DOMAIN'],
+            'headers': request.headers,
+            'body': request.body,
+            'query_params': request.GET,
+            'method': request.method,
 
-        logging.info(f"status code {response.status_code}")
+        }
+
+        logging.info(request_log)
+
+
+        response = self.get_response(request)
+
+        logging.info(f"status code response {response.status_code}")
         return response
