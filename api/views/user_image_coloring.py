ed2220553dbf1b35015810ed76c3302e09f137f9
Dmitriy <mdmgrand@mail.ru>
2024-08-14 22:02:49 +0300

    updated UserColoringDetailUpdateView - patch/delete methods

--- api/views/user_image_coloring.py
+++ api/views/user_image_coloring.py
@@ -7,9 +7,11 @@ from service_objects.services import ServiceOutcome
 from api.serializers.image.user_image import UserColoringSerializer
 from api.serializers.theme.list import ThemeListSerializer
 from api.services.user_colorings.get import UserColoringGetService
+from api.services.user_colorings.update import UserColoringUpdateService
+from models_app.models import UserColoring
 
 
-class UserColoringDetailView(APIView):
+class UserColoringDetailUpdateView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
@@ -23,3 +25,18 @@ class UserColoringDetailView(APIView):
             },
             status=status.HTTP_200_OK
         )
+
+    def patch(self, request, *args, **kwargs):
+        ServiceOutcome(UserColoringUpdateService, {
+            'user_coloring_id': kwargs['id'], 'user_id': request.user.id,
+            'coloring_json': request.data.get('coloring_json'),
+        },
+                       {'image': request.data.get('image')})
+        return Response(status=status.HTTP_201_CREATED)
+
+    def delete(self, request, *args, **kwargs):
+        delete_user_coloring = UserColoring.objects.get(
+            id=kwargs['id']
+        )
+        delete_user_coloring.delete()
+        return Response(status=204)
